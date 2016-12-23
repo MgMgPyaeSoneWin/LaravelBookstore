5e4669cc8cda22887edb2855adffc2d58def9c68
Pyae Sone <mgmgpyaesonewin@gmail.com>
2016-12-23 12:31:53 +0630

    Data Seeds

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        // Available alpha caracters
+        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // generate a pin based on 2 * 7 digits + a random character
+        $pin = mt_rand(1000000, 9999999)
+            . mt_rand(1000000, 9999999)
+            . $characters[rand(0, strlen($characters) - 1)];
+
+        // shuffle the result
+        $code_no = str_shuffle($pin);
+
+        DB::table('books')->insert([
+            'code_no' => $code_no,
+            'name' => str_random(10).' '.str_random(10),
+            'price' => bcrypt('secret'),
+        ]);
+    }
+}
