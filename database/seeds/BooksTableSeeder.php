04c07e498652d1f1b0f0b3d732fd8d0a9aa53caa
Pyae Sone <mgmgpyaesonewin@gmail.com>
2016-12-31 16:22:36 +0630

    Data Seeeded

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -11,42 +11,125 @@ class BooksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
-        // Available alpha caracters
-//        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-        // generate a pin based on 2 * 7 digits + a random character
-//        $pin = mt_rand(1000000, 9999999)
-//            . mt_rand(1000000, 9999999)
-//            . $characters[rand(0, strlen($characters) - 1)];
-
-        // shuffle the result
-//        $code_no = str_shuffle($pin);
-
-//        $faker = Faker\Factory::create();
-//        $limit = 10;
-//        for ($i = 0; $i < $limit; $i++) {
-//            DB::table('books')->insert([
-//                'code_no' => $code_no,
-//                'name' => $faker->name,
-//                'author_id' => $faker->randomNumber(1)+1,
-//                'publisher_id' => $faker->randomNumber(1)+1,
-//                'genre_id' => $faker->randomNumber(1)+1,
-//                'price' => $faker->randomNumber(2)."\$",
-//                'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
-//                'edition' => $faker->randomNumber(1),
-//                'deleted' => 0
-//            ]);
+        $faker = Faker\Factory::create();
         DB::table('books')->insert([
-                'code_no' => 'A001',
-                'name' => 'Spam Nation: The Inside Story of Organized Cybercrime-from Global Epidemic to Your Front Door',
-                'author_id' => $faker->randomNumber(1)+1,
-                'publisher_id' => $faker->randomNumber(1)+1,
-                'genre_id' => $faker->randomNumber(1)+1,
-                'price' => $faker->randomNumber(2)."\$",
-                'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
-                'edition' => $faker->randomNumber(1),
-                'deleted' => 0
-            ]);
+            'code_no' => 'A001',
+            'name' => 'Spam Nation: The Inside Story of Organized Cybercrime-from Global Epidemic to Your Front Door',
+            'author_id' => 1,
+            'publisher_id' => 1,
+            'genre_id' => 1,
+            'price' => "$11.87",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A002',
+            'name' => 'The Everything Store: Jeff Bezos and the Age of Amazon',
+            'author_id' => 2,
+            'publisher_id' => 1,
+            'genre_id' => 2,
+            'price' => "$19.14",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A003',
+            'name' => 'The Upstarts: How Uber, Airbnb, and the Killer Companies of the New Silicon Valley Are Changing the World',
+            'author_id' => 2,
+            'publisher_id' => 2,
+            'genre_id' => 2,
+            'price' => "$30.00",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A004',
+            'name' => 'Data-ism: The Revolution Transforming Decision Making, Consumer Behavior, and Almost Everything Else',
+            'author_id' => 3,
+            'publisher_id' => 2,
+            'genre_id' => 3,
+            'price' => "$23.92",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A005',
+            'name' => 'Dit is big data (Dutch Edition)',
+            'author_id' => 3,
+            'publisher_id' => 3,
+            'genre_id' => 3,
+            'price' => "$15.66",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A006',
+            'name' => 'SEO 2017: Learn search engine optimization with smart internet marketing strategies',
+            'author_id' => 4,
+            'publisher_id' => 3,
+            'genre_id' => 4,
+            'price' => "$49.99",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A007',
+            'name' => 'Work Rules!: Insights from Inside Google That Will Transform How You Live and Lead',
+            'author_id' => 5,
+            'publisher_id' => 3,
+            'genre_id' => 2,
+            'price' => "$6.76",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A008',
+            'name' => 'Code: The Hidden Language of Computer Hardware and Software',
+            'author_id' => 6,
+            'publisher_id' => 4,
+            'genre_id' => 5,
+            'price' => "$86.55",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A009',
+            'name' => 'CApplications = Code + Markup: A Guide to the Microsoft® Windows® Presentation Foundation: A Guide to the Microsoft Windows Presentation Foundation (Developer Reference)',
+            'author_id' => 6,
+            'publisher_id' => 4,
+            'genre_id' => 5,
+            'price' => "$59.99",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
+
+        DB::table('books')->insert([
+            'code_no' => 'A010',
+            'name' => 'The Annotated Turing: A Guided Tour Through Alan Turing\'s Historic Paper on Computability and the Turing Machine',
+            'author_id' => 6,
+            'publisher_id' => 4,
+            'genre_id' => 5,
+            'price' => "$22.48",
+            'publishing_date' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'edition' => $faker->randomNumber(1),
+            'deleted' => 0
+        ]);
     }
 }
